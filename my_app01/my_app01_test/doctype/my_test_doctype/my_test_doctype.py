b9ed0efe4069f7a4049a9d3110d6310bc6b6e9d3
ndcatabona <152581511+ndcatabona@users.noreply.github.com>
2024-10-11 02:41:12 +0000

    This is a test doctype

---
+++ my_app01/my_app01_test/doctype/my_test_doctype/my_test_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Noel D. Catabona and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MyTestDocType(Document):
+	pass
